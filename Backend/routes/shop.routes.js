f7fca48b63b107850b4c9885a4ced7721087ed15
AakashSudheendra <aakash151205@gmail.com>
2026-04-21 21:33:02 +0530

    item controller and routes are created

--- Backend/routes/shop.routes.js
+++ Backend/routes/shop.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { createAndEditShop } from "../controllers/shop.controller";
 import isAuth from "../middlewares/isAuth";
+import { upload } from "../middlewares/multer.js";
 
 
 //user routes
 const shopRouter = express.Router();
 
-shopRouter.get("/create-edit",isAuth, createAndEditShop);
+shopRouter.post("/create-edit",isAuth,upload.single("image"), createAndEditShop);
 
 
 
